61bc6bf7db7b50050c18ba9e7f4b9c772ba27460
D Otasek <dotasek.dev@gmail.com>
2020-10-06 15:54:18 -0400

    Remove unused code from example

--- example/index.tsx
+++ example/index.tsx
@@ -2,7 +2,6 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import { useState } from 'react'
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
 import { CyNDExProvider } from '../.';
 import { NDExAccountProvider } from '../.';
 import { OpenInCytoscapeButton } from '../.';
